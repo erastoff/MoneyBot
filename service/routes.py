2f0270b468f25eb3a15cc0c80e10d521cafc653a
erastoff <yuriy.erastov@yandex.ru>
2024-04-27 20:22:25 +0300

    Master - get_pair_rates() route added

--- service/routes.py
+++ service/routes.py
@@ -1,16 +1,20 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-s
 from typing import Annotated
 
 from aiogram import types
 from fastapi import APIRouter, Depends, Header
 from loguru import logger
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette.exceptions import HTTPException
 
+from keyboards.calculation_keyboards import TICKERS
+from .binance_api import set_cache_binance_rates
 from .bot import bot, dp
 from orm import crud
 from orm.database import get_session
 from settings import get_settings
+from .redis_pool import pool
 
 cfg = get_settings()
 
@@ -19,6 +23,11 @@ root_router = APIRouter(
     tags=["root"],
     responses={404: {"description": "Not found"}},
 )
+
+
+class Rate(BaseModel):
+    pair: str
+    price: float
 
 
 @root_router.get("/")
@@ -32,6 +41,18 @@ async def read_user(user_id: int, db: Session = Depends(get_session)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@root_router.get("/rates/{pair}")
+async def get_pair_rates(pair: str):
+    if pair in TICKERS:
+        raise HTTPException(status_code=404, detail=f"{pair} not found!")
+    await set_cache_binance_rates()
+    response_value = await pool.get(pair)
+    if not response_value:
+        raise HTTPException(status_code=404, detail=f"{pair} not found!")
+    tmp = Rate(pair=pair, price=response_value)
+    return tmp
 
 
 @root_router.post(cfg.webhook_path)
