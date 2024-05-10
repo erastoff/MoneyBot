02c31cc634d576bd9aa5e72bcac280f26cf0d423
erastoff <yuriy.erastov@yandex.ru>
2024-05-10 16:19:00 +0300

    Master - delete callback handlers

--- handlers/callback_handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-# from aiogram import Router
-#
-# from .common_callback_handler import router as common_callback_router
-#
-# router = Router(name=__name__)
-#
-# router.include_routers(common_callback_router)
